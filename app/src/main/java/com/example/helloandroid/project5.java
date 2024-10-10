90a20b0f7d970df0bbb9fd159129b488f5998191
SeoYoung2001 <ghok111@naver.com>
2024-10-10 16:42:24 +0900

    commit test

--- app/src/main/java/com/example/helloandroid/project5.java
+++ app/src/main/java/com/example/helloandroid/project5.java
@@ -8,18 +8,14 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Button;
-
-public class MainActivity extends AppCompatActivity {
-
-
-
-    public void onCreate(Bundle savedInstanceState){
+public class project5 extends AppCompatActivity{
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.project4_3);
+        setContentView(R.layout.project5_1);
         setContentView(R.layout.project5_2);
-        setContentView(R.layout.project5_4);
 
 
+    }
 
 
 }
---
+++ app/src/main/java/com/example/helloandroid/project5_3.java
@@ -0,0 +1,53 @@
+package com.example.helloandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class project5_3 extends AppCompatActivity {
+    private EditText edit;
+    private Button btn;
+    private TextView text;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+
+        LinearLayout baseLayout = new LinearLayout(this);
+        baseLayout.setOrientation(LinearLayout.VERTICAL);
+        setContentView(baseLayout,params);
+
+        edit = new EditText(this);
+        baseLayout.addView(edit);
+
+        btn = new Button(this);
+        btn.setText("버튼입니다");
+        btn.setBackgroundColor(Color.YELLOW);
+        baseLayout.addView(btn);
+
+        text = new TextView(this);
+        text.setTextSize(20);
+        text.setTextColor(Color.MAGENTA);
+        baseLayout.addView(text);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText(edit.getText().toString());
+            }
+        });
+    }
+
+
+
+
+
+
+}
